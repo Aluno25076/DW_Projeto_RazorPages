482fb0385cf5646ae4a7d01ca46d381d31cb8973
Aluno25076 <aluno25076@ipt.pt>
2026-05-22 10:15:36 +0100

    Criar Projeto

---
+++ DW_Projeto_RazorPages/DW_Projeto_RazorPages/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DW_Projeto_RazorPages.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
---
+++ DW_Projeto_RazorPages/DW_Projeto_RazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DW_Projeto_RazorPages.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
