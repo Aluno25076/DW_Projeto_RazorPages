2f665d184bc416e2c038bbe8e5b4633e792e4090
Aluno25076 <aluno25076@ipt.pt>
2026-06-18 10:19:11 +0100

    criação das pages AppUsers, subscriptions e members

---
+++ DW_Projeto_RazorPages/DW_Projeto_RazorPages/Pages/Members/Delete.cshtml.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using DW_Projeto_RazorPages.Data.Model;
+using DW_Projeto_RazorPages.Data;
+
+namespace DW_Projeto_RazorPages.Pages.MemberPages;
+
+public class DeleteModel : PageModel
+{
+    private readonly ApplicationDbContext _context;
+
+    public DeleteModel(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    [BindProperty]
+    public Member Member { get; set; } = default!;
+
+    public async Task<IActionResult> OnGetAsync(int? memberid)
+    {
+        if (memberid is null)
+        {
+            return NotFound();
+        }
+
+        var member = await _context.Members.FirstOrDefaultAsync(m => m.MemberId == memberid);
+        if (member is null)
+        {
+            return NotFound();
+        }
+        else
+        {
+            Member = member;
+        }
+
+        return Page();
+    }
+
+    public async Task<IActionResult> OnPostAsync(int? memberid)
+    {
+        if (memberid is null)
+        {
+            return NotFound();
+        }
+
+        var member = await _context.Members.FindAsync(memberid);
+        if (member != null)
+        {
+            Member = member;
+            _context.Members.Remove(Member);
+            await _context.SaveChangesAsync();
+        }
+
+        return RedirectToPage("./Index");
+    }
+}
