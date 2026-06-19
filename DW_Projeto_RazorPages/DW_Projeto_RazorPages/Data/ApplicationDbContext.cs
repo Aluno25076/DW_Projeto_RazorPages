2b04c08ac2329dc8d418bb3ac638d4d9aec049c0
Aluno25076 <aluno25076@ipt.pt>
2026-06-19 16:32:24 +0100

    reciar proj

--- DW_Projeto_RazorPages/DW_Projeto_RazorPages/Data/ApplicationDbContext.cs
+++ DW_Projeto_RazorPages/DW_Projeto_RazorPages/Data/ApplicationDbContext.cs
@@ -1,14 +1,10 @@
-﻿using DW_Projeto_RazorPages.Data.Model;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DW_Projeto_RazorPages.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
     {
-        public DbSet<MyUser> AppUsers { get; set; }
-        public DbSet<Member> Members { get; set; }
-        public DbSet<Subscription> Subscriptions { get; set; }
-        public DbSet<Subscribed> Subscribers { get; set; }
+    public DbSet<DW_Projeto_RazorPages.Data.Model.Subscription> Subscription { get; set; } = default!;
     }
 }
--- DW_Projeto_RazorPages/DW_Projeto_RazorPages/Data/Migrations/20260619152628_Models.Designer.cs
+++ DW_Projeto_RazorPages/DW_Projeto_RazorPages/Data/Migrations/20260619152628_Models.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DW_Projeto_RazorPages.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20260619105902_Models")]
+    [Migration("20260619152628_Models")]
     partial class Models
     {
         /// <inheritdoc />
@@ -24,80 +24,6 @@ namespace DW_Projeto_RazorPages.Data.Migrations
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
-
-            modelBuilder.Entity("DW_Projeto_RazorPages.Data.Model.MyUser", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<DateOnly>("BirthDate")
-                        .HasColumnType("date");
-
-                    b.Property<string>("CellPhone")
-                        .HasMaxLength(19)
-                        .HasColumnType("nvarchar(19)");
-
-                    b.Property<string>("Discriminator")
-                        .IsRequired()
-                        .HasMaxLength(8)
-                        .HasColumnType("nvarchar(8)");
-
-                    b.Property<string>("Name")
-                        .IsRequired()
-                        .HasMaxLength(50)
-                        .HasColumnType("nvarchar(50)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("AppUsers");
-
-                    b.HasDiscriminator<string>("Discriminator").HasValue("MyUser");
-
-                    b.UseTphMappingStrategy();
-                });
-
-            modelBuilder.Entity("DW_Projeto_RazorPages.Data.Model.Subscribed", b =>
-                {
-                    b.Property<int>("MemberFK")
-                        .HasColumnType("int");
-
-                    b.Property<int>("SubscriptionFK")
-                        .HasColumnType("int");
-
-                    b.Property<DateTime>("ExpirationDate")
-                        .HasColumnType("datetime2");
-
-                    b.HasKey("MemberFK", "SubscriptionFK");
-
-                    b.HasIndex("SubscriptionFK");
-
-                    b.ToTable("Subscribers");
-                });
-
-            modelBuilder.Entity("DW_Projeto_RazorPages.Data.Model.Subscription", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<decimal>("Fee")
-                        .HasPrecision(9, 2)
-                        .HasColumnType("decimal(9,2)");
-
-                    b.Property<string>("Program")
-                        .IsRequired()
-                        .HasMaxLength(300)
-                        .HasColumnType("nvarchar(300)");
-
-                    b.HasKey("Id");
-
-                    b.ToTable("Subscriptions");
-                });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRole", b =>
                 {
@@ -301,47 +227,6 @@ namespace DW_Projeto_RazorPages.Data.Migrations
                     b.ToTable("AspNetUserTokens", (string)null);
                 });
 
-            modelBuilder.Entity("DW_Projeto_RazorPages.Data.Model.Member", b =>
-                {
-                    b.HasBaseType("DW_Projeto_RazorPages.Data.Model.MyUser");
-
-                    b.Property<int>("MemberId")
-                        .HasColumnType("int");
-
-                    b.Property<DateTime>("RegistrationDate")
-                        .HasColumnType("datetime2");
-
-                    b.Property<string>("SubscribedFK")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<int?>("SubscriptionId")
-                        .HasColumnType("int");
-
-                    b.HasIndex("SubscriptionId");
-
-                    b.HasDiscriminator().HasValue("Member");
-                });
-
-            modelBuilder.Entity("DW_Projeto_RazorPages.Data.Model.Subscribed", b =>
-                {
-                    b.HasOne("DW_Projeto_RazorPages.Data.Model.Member", "Member")
-                        .WithMany()
-                        .HasForeignKey("MemberFK")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.HasOne("DW_Projeto_RazorPages.Data.Model.Subscription", "Subscription")
-                        .WithMany()
-                        .HasForeignKey("SubscriptionFK")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
-
-                    b.Navigation("Member");
-
-                    b.Navigation("Subscription");
-                });
-
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
                 {
                     b.HasOne("Microsoft.AspNetCore.Identity.IdentityRole", null)
@@ -391,18 +276,6 @@ namespace DW_Projeto_RazorPages.Data.Migrations
                         .HasForeignKey("UserId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
-                });
-
-            modelBuilder.Entity("DW_Projeto_RazorPages.Data.Model.Member", b =>
-                {
-                    b.HasOne("DW_Projeto_RazorPages.Data.Model.Subscription", null)
-                        .WithMany("MembersList")
-                        .HasForeignKey("SubscriptionId");
-                });
-
-            modelBuilder.Entity("DW_Projeto_RazorPages.Data.Model.Subscription", b =>
-                {
-                    b.Navigation("MembersList");
                 });
 #pragma warning restore 612, 618
         }
