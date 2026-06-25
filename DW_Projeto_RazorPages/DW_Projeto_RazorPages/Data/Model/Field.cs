5a92ae6b35976f1be1f45cc63a77c74243cd8935
aluno23785 <aluno23785@ipt.pt>
2026-06-25 06:12:16 +0100

    Modelos em que estavam em falta, Field, Match e Employee

---
+++ DW_Projeto_RazorPages/DW_Projeto_RazorPages/Data/Model/Field.cs
@@ -0,0 +1,52 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DW_Projeto_RazorPages.Data.Model
+{
+    /// <summary>
+    /// Classe para representar os campos de ténis
+    /// que existem no clube
+    /// </summary>
+            public class Field
+            {/// <summary>
+             /// Chave Primaria (PK)
+             /// </summary>
+                [Key]
+                public int Id { get; set; }
+
+   
+   
+
+            // Dimensões do campo (máximo de 9 caracteres, com máscara, exemplo: "64x32" em metros)
+            [Required(ErrorMessage = "As dimensões do campo são obrigatórias.")]
+            [StringLength(9, ErrorMessage = "As dimensões não podem exceder 9 caracteres.")]
+            [Display(Name = "Dimensões")]
+            public string Size { get; set; } = string.Empty;
+
+            // Número  do campo no clube 
+            [Display(Name = "Número do Campo")]
+            public int? Number { get; set; }
+
+            // Tipo de superfície do campo 
+            [Required(ErrorMessage = "O tipo de campo é obrigatório.")]
+            [Display(Name = "Tipo de superfície e cobertura")]
+            public FieldType Type { get; set; }
+
+
+            /// <summary>
+            /// atributo para demonstrar o tipo de campo
+            /// </summary>
+            public enum FieldType
+        {
+            // Piso de terra batida
+            Clay,
+            // Piso de relva sintética
+            SyntheticGrass,
+            // Piso de alcatrão 
+            HardCourt,
+            // Piso de relva natural
+            Grass,
+            // Campo coberto 
+            Indoor
+        }
+    }
+}
---
+++ DW_Projeto_RazorPages/DW_Projeto_RazorPages/Data/Model/Match.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DW_Projeto_RazorPages.Data.Model
+{
+
+
+    /// <summary>
+    /// Classe para representar as partidas organizadas e completadas da aplicação
+    /// estes dados demonstram os resultados das partidas, participantes , o campo em que foi jogada e vencedores
+    /// </summary>
+    public class Match
+    {
+        /// <summary>
+        /// Chave Primaria (PK)
+        /// </summary>
+        [Key]
+        public int Id { get; set; }
+
+       
+
+        /// <summary>
+        /// dia da partida
+        /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatorio!")]
+        [Display(Name = "Data em que a partida decorreu ou vai ocorrer")]
+        public DateOnly Day { get; set; }
+
+
+        /// <summary>
+        /// campo da partida
+        /// </summary>
+
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatorio!")]
+        [Display(Name = "ID do campo da partida")]
+        public string Field { get; set; } = "";
+
+
+    }
+}
