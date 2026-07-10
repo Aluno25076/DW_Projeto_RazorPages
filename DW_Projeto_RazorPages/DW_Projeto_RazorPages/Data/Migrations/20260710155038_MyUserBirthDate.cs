789a55a5b94fcceb84f116b557370c27c2e3b7af
Aluno25076 <aluno25076@ipt.pt>
2026-07-10 17:19:24 +0100

    adicionar o entity á data de nascimento dos utilizadores, migração e update da base de dados

---
+++ DW_Projeto_RazorPages/DW_Projeto_RazorPages/Data/Migrations/20260710155038_MyUserBirthDate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DW_Projeto_RazorPages.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MyUserBirthDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
